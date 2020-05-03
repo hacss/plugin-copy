a7675d4e6a971e3e93f504462057deee45c3b6ba
Nick Saunders <nick@saunde.rs>
2020-05-03 09:12:33 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+  <a href="https://hacss.io" target="_blank"><img width="256" src="https://raw.githubusercontent.com/hacss/assets/master/logos/hacss-black.svg"></a><br>
+  Copy Plugin
+</p>
+
+<p align="center">
+    <a href="https://travis-ci.org/hacss/hacss"><img src="https://img.shields.io/travis/hacss/plugin-copy/master.svg" alt="Build Status"></a>
+    <a href="https://github.com/hacss/hacss/releases"><img src="https://img.shields.io/npm/v/@hacss/plugin-copy.svg" alt="Latest Release"></a>
+    <a href="https://github.com/hacss/hacss/blob/master/LICENSE"><img src="https://img.shields.io/npm/l/@hacss/plugin-copy.svg" alt="License"></a>
+</p>
---
+++ test/index.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+const copyPlugin = require("../index.js");
+
+describe("copy plugin", () => {
+  const input = {
+    "margin-x": "20px",
+    "margin-y": "30px",
+  };
+
+  const spec = {
+    "margin-x": ["margin-left", "margin-right"],
+    "margin-y": ["margin-top", "margin-bottom"],
+  };
+
+  const [ copy, properties ] = copyPlugin(spec);
+
+  it("copies values to specified properties", () => {
+    expect(copy(input)).to.deep.equal({
+      ...input,
+      "margin-left": "20px",
+      "margin-right": "20px",
+      "margin-top": "30px",
+      "margin-bottom": "30px",
+    });
+  });
+
+  it("adds the keys from the spec to the list of recognized properties", () => {
+    expect(properties).to.deep.equal(Object.keys(spec));
+  });
+});
