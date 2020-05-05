425bcdb6418c343f67803057dac558d92a82e5eb
Nick Saunders <nick@saunde.rs>
2020-05-05 15:35:37 -0700

    Fix bug with extra properties being set.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ export default spec => {
     decls => {
       from.forEach(from => {
         spec[from].forEach(to => {
-          decls[to] = decls[from];
+          if (from in decls) {
+            decls[to] = decls[from];
+          }
         });
       });
       return decls;
--- test/index.js
+++ test/index.js
@@ -9,31 +9,34 @@ const input = {
 const spec = {
   "margin-x": ["margin-left", "margin-right"],
   "margin-y": ["margin-top", "margin-bottom"],
+  "padding-x": ["padding-left", "padding-right"],
 };
 
 const [copy, properties] = copyPlugin(spec);
 
-{
-  const expected = {
-    ...input,
-    "margin-left": "20px",
-    "margin-right": "20px",
-    "margin-top": "30px",
-    "margin-bottom": "30px",
-  };
+const test = (actual, expected) => {
+  console.log(`${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+  assert.deepEqual(actual, expected);
+};
 
+test(copy(input), {
+  ...input,
+  "margin-left": "20px",
+  "margin-right": "20px",
+  "margin-top": "30px",
+  "margin-bottom": "30px",
+});
+
+test(properties, Object.keys(spec));
+
+{
   const actual = copy(input);
-
-  console.log(`${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-  assert.deepEqual(actual, expected);
-}
-
-{
-  const expected = Object.keys(spec);
-  const actual = properties;
-
-  console.log(`${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
-  assert.deepEqual(actual, expected);
+  console.log(`!"padding-left" in ${actual}`);
+  if ("padding-left" in actual) {
+    assert.fail(
+      "Found padding-left where it was expected not to have been set.",
+    );
+  }
 }
 
 console.log("All tests passed.");
